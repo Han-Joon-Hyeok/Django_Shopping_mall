2bf83a82c64b196a560ca7444dcd0b037493ffc0
Han-Joon-Hyeok <virq@naver.com>
2020-11-15 23:52:21 +0900

    회원가입 및 로그인/로그아웃 구현

--- account/views.py
+++ account/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib import auth
+from account.models import User
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        user_id = request.POST['user_id']
+        password = request.POST['password']
+        user = auth.authenticate(request, user_id=user_id, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('main')
+        else:
+            return render(request, 'login.html', {'error': 'wronginfo'})
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('main')
+
+def register(request) :
+    if request.method == "POST" :
+        if request.POST["password"] == request.POST["password_confirm"] :
+            try :
+                user = User.objects.create_user(
+                    user_id = request.POST["user_id"],
+                    username = request.POST["username"],
+                    email = request.POST["email"],
+                    address = request.POST["address"],
+                    password = request.POST["password"]
+                )
+                user.save()
+                return redirect('main')
+            except :
+                return render(request, 'register.html')
+    
+    else:
+        return render(request, 'register.html')
